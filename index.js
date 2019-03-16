85fabd92e33a7fcb9c2c3bfb65bdab0ef61992a4
Chris Horn <chrishornuk@gmail.com>
2019-03-16 18:35:09 +0000

    basic form and menu working

--- index.js
+++ index.js
@@ -5,14 +5,95 @@ exports.Service = function (apiKey, serviceName, verbs) {
     this.apiKey = apiKey;
     this.serviceName = serviceName;
     this.verbs = verbs;
+    this.menus = [];
+    this.forms = [];
 }
 
-exports.Service.prototype.renderMenu = function (template, data) {
-    var html = pug.renderFile(template, data);
+function Form(index, template, data) {
+    this.template = template;
+    this.data = data;
+    this.index = index;
+    this.type = "form";
+}
+
+Form.prototype.header = function (header) {
+    if (typeof header !== 'undefined') {
+        this.headerValue = header;
+        return true;
+    } else {
+        return this.headerValue;
+    }
+}
+
+Form.prototype.render = function () {
+
+    var html = pug.renderFile(this.template, this.data);
     var root = parse(html);
     var result = {};
 
-    var body = [];
+    result.type = this.type;
+    result.header = this.header();
+    result.body = [];
+    result.nextRoute = root.childNodes[0].attributes['submit'];
+    result.method = root.childNodes[0].attributes['method'].toUpperCase();
+
+    var form = root.childNodes[0];
+
+    for (var i = 0; i < form.childNodes.length; i++) {
+        var record = undefined;
+        switch (form.childNodes[i].tagName) {
+            case "p":
+                record = {};
+                try {
+                    record.description = typeof form.childNodes[i].text !== 'undefined' ?
+                        form.childNodes[i].text
+                        :
+                        undefined;
+                    record.name = typeof form.childNodes[i].childNodes[1] !== 'undefined' ?
+                        form.childNodes[i].childNodes[1].attributes['name']
+                        :
+                        undefined;
+                    record.type = typeof form.childNodes[i].childNodes[1] !== 'undefined' ?
+                        form.childNodes[i].childNodes[1].attributes['type']
+                        :
+                        undefined;
+                } catch (error) {
+                    record = {};
+                }
+                break;
+            default:
+                break;
+        }
+        if (record) result.body.push(record);
+    }
+    return result;
+}
+
+function Menu(index, template, data) {
+    this.template = template;
+    this.data = data;
+    this.index = index;
+    this.type = "menu";
+}
+
+Menu.prototype.header = function (header) {
+    if (typeof header !== 'undefined') {
+        this.headerValue = header;
+        return true;
+    } else {
+        return this.headerValue;
+    }
+}
+
+Menu.prototype.render = function () {
+
+    var html = pug.renderFile(this.template, this.data);
+    var root = parse(html);
+    var result = {};
+
+    result.type = this.type;
+    result.header = this.header();
+    result.body = [];
     for (var i = 0; i < root.childNodes.length; i++) {
         var record = undefined;
         switch (root.childNodes[i].tagName) {
@@ -34,10 +115,21 @@ exports.Service.prototype.renderMenu = function (template, data) {
             default:
                 break;
         }
-        if (record) body.push(record);
+        if (record) result.body.push(record);
     }
-    result.json = body;
     return result;
+}
+
+exports.Service.prototype.addForm = function (template, data) {
+    var form = new Form(this.forms.length, template, data);
+    this.forms.push(form);
+    return this.forms[this.forms.length - 1];
+}
+
+exports.Service.prototype.addMenu = function (template, data) {
+    var menu = new Menu(this.menus.length, template, data);
+    this.menus.push(menu);
+    return this.menus[this.menus.length - 1];
 }
 
 exports.Service.prototype.renderForm = function (template, data) {
